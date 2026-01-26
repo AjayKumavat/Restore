e80c14b60aaab453d6c27032451ce1a9adf516b8
AjayKumavat <akmvt0@gmail.com>
2026-01-26 16:55:55 +0530

    End of section 5, Implement Redux, RTK query,  for state management, Implement persistant dark mode, Implement liner loader

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,13 +7,18 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/routes/Routes';
+import { Provider } from 'react-redux';
+import { store } from './app/store/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider 
+    <Provider store={store}>
+      <RouterProvider 
       router={router} 
       // for avoiding warning messages about future features (inspect -> console tab)
       // future={{ v7_startTransition: true }} 
     />
+    </Provider>
+    
   </StrictMode>,
 )
